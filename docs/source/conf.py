e8d06494104e6892916add0a9d29a6159fcb5087
Edward Linscott <edwardlinscott@gmail.com>
2023-07-20 14:31:24 +0200

    Bump version: 0.0.1-dev → 0.0.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ copyright = f"{date.today().year}, Edward Linscott"
 author = "Edward Linscott"
 
 # The full version, including alpha/beta/rc tags.
-release = "0.0.1-dev"
+release = "0.0.1"
 
 # The short X.Y version.
 parsed_version = re.match(
